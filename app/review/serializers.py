302002848343eccb843a89401efcf7948ce78d6f
Andre Martins <andremartins@tvglobo.com.br>
2019-05-03 17:20:06 -0300

    Adds resorce to get remote ip address in serializer

--- app/review/serializers.py
+++ app/review/serializers.py
@@ -10,3 +10,10 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'rating', 'summary', 'ip_address',
                   'submission_date', 'company')
         read_only_fields = ['id', ]
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        remote_id = request.META['REMOTE_ADDR']
+        validated_data['ip_address'] = remote_id
+
+        return super().create(validated_data)
